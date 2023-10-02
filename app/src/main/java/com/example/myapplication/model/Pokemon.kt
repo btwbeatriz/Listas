658131a805b1d6da46e88373495969add9b4d24b
btwbeatriz <beatriz.ffreitas@hotmail.com>
2023-10-02 10:01:08 -0300

    Aula 02/10/23

--- app/src/main/java/com/example/myapplication/model/Pokemon.kt
+++ app/src/main/java/com/example/myapplication/model/Pokemon.kt
@@ -1,4 +1,4 @@
-package com.example.myapplication
+package com.example.myapplication.model
 
 data class PokemonResponse(
     val count: Int? = 0,
--- app/src/main/java/com/example/myapplication/view/SecondActivity.kt
+++ app/src/main/java/com/example/myapplication/view/SecondActivity.kt
@@ -1,10 +1,11 @@
-package com.example.myapplication
+package com.example.myapplication.view
 
 import android.content.Context
 import android.os.Build
 import android.os.Bundle
 import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
+import com.example.myapplication.model.Usuario
 import com.example.myapplication.databinding.ActivitySecondBinding
 import com.google.gson.Gson
 
@@ -20,12 +21,13 @@ class SecondActivity : AppCompatActivity() {
 
         setContentView(binding.root)
         val usuario = intent.extras?.getParcelable("USUARIO", Usuario::class.java)
-
         val prefs = this.getSharedPreferences("", Context.MODE_PRIVATE)
 
-        val usuarioFromJson = Gson().fromJson(
-            prefs.getString("USUARIO", "Deu ruim ao buscar o dado"),
-            Usuario::class.java)
+        val usuarioFromJson =
+            Gson().fromJson(
+                prefs.getString("USUARIO", "Deu ruim ao buscar o dado"),
+                Usuario::class.java
+            )
 
         val cursoNome = prefs.getString("USUARIO", "Deu ruim ao buscar o dado")
 
