5656cc011dc4f71c1ff247812546ced23dcc0486
btwbeatriz <beatriz.ffreitas@hotmail.com>
2023-10-20 10:07:51 -0300

    Aula 20/10/23

--- app/src/main/java/com/example/myapplication/view/SecondActivity.kt
+++ app/src/main/java/com/example/myapplication/view/SecondActivity.kt
@@ -4,7 +4,10 @@ import android.os.Build
 import android.os.Bundle
 import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
+import com.bumptech.glide.Glide
+import com.example.myapplication.R
 import com.example.myapplication.databinding.ActivitySecondBinding
+import com.example.myapplication.model.PokemonCompleteData
 
 class SecondActivity : AppCompatActivity() {
 
@@ -19,8 +22,21 @@ class SecondActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         val pokemon =
-            intent.getStringExtra("POKEMON_COMPLETE_DATA")
+            intent
+                ?.extras
+                ?.getParcelable<PokemonCompleteData>(
+                    "POKEMON_COMPLETE_DATA"
+                )
 
-        binding.textViewNomeDoUsuario.text = pokemon
+        binding.textViewName.text = pokemon?.name
+        binding.textViewNumber.text = getString(R.string.pokemon_order, pokemon?.id.toString())
+        binding.textViewHeight.text =
+            getString(R.string.pokemon_height, pokemon?.height.toString())
+        binding.textViewWidth.text = getString(R.string.pokemon_weight, pokemon?.weight.toString())
+
+        Glide
+            .with(this)
+            .load(pokemon?.sprites?.frontDefault)
+            .into(binding.imageViewSprite)
     }
 }
